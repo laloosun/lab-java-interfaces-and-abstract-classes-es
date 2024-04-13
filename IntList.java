1361e3a1a97bb5a3421b00ff2539888507119ab5
laloosun <104689581+laloosun@users.noreply.github.com>
2024-04-13 12:18:50 +0100

    InterfaceIntList

---
+++ IntList.java
@@ -0,0 +1,9 @@
+// IntList.java
+// This interface defines the behavior of an IntList.
+
+package com.example.intlist;
+
+public interface IntList {
+    void add(int number);
+    int get(int id);
+}
